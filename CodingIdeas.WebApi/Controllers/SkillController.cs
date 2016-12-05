510d34e98db255ce7e9a5a566ea490b2050344cf
vstrimaitis <vstrimaitis@gmail.com>
2016-12-05 15:41:54 +0200

    Added controller for user skills.

---
+++ CodingIdeas.WebApi/Controllers/SkillController.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Web.Http;
+
+namespace CodingIdeas.WebApi.Controllers
+{
+    public class SkillController : ApiController
+    {
+        [HttpPost]
+        [Route("api/Skill")]
+        public IHttpActionResult Post([FromBody]JObject value)
+        {
+            var template = new { UserId = Guid.Empty, LanguageId = Guid.Empty, Proficiency = (byte)0 };
+            var skill = JsonConvert.DeserializeAnonymousType(value.ToString(), template);
+            var mgr = WebApiApplication.Manager;
+            mgr.AddSkill(skill.UserId, skill.LanguageId, skill.Proficiency);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("api/Skill")]
+        public IHttpActionResult Put([FromBody]JObject value)
+        {
+            var template = new { UserId = Guid.Empty, LanguageId = Guid.Empty, Proficiency = (byte)0 };
+            var skill = JsonConvert.DeserializeAnonymousType(value.ToString(), template);
+            var mgr = WebApiApplication.Manager;
+            mgr.UpdateSkill(skill.UserId, skill.LanguageId, skill.Proficiency);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("api/Skill/{userId}/{languageId}")]
+        public IHttpActionResult Delete(Guid userId, Guid languageId)
+        {
+            var mgr = WebApiApplication.Manager;
+            mgr.RemoveSkill(userId, languageId);
+            return Ok();
+        }
+    }
+}
